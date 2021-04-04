61bfc77378c3e2aeb4b57c54608df35da1510b8f
KowalDrzo <61516230+KowalDrzo@users.noreply.github.com>
2021-04-05 00:01:26 +0200

    Działa wykrywanie, czy to poprawny plik JPG

--- parser.py
+++ parser.py
@@ -1,4 +1,10 @@
 
+class PictureInfo:
+
+    SOI = False
+
+
+
 """
 Funkcja parse_jpg służy do parsowania pliku jpg celem wyciągnięcia informacji o obrazie
 """
@@ -6,7 +12,15 @@ Funkcja parse_jpg służy do parsowania pliku jpg celem wyciągnięcia informacj
 def parse_jpg(file_name):
 
     file = open(file_name, "rb")
-    content = file.read()
-    file.close()
+    
+    pic_inf = PictureInfo()
 
-    print (content)
+    chunk_name = file.read(2)
+    int_byte = int.from_bytes(chunk_name, "big")
+
+    if int_byte == 0xffd8:
+        print("To jest JPG")
+    else:
+        print("To NIE jest JPG")
+
+    file.close()
