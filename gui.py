74adc0e669616cbe927256eca5ff220840257949
KowalDrzo <61516230+KowalDrzo@users.noreply.github.com>
2021-04-08 16:13:09 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -15,14 +15,15 @@ class GuiMenu:
     def display_menu(self):
         
         main_window = Tk()
-        main_window.geometry("350x300")
+        main_window.geometry("640x480")
         main_window.title("JPEG Parser")
         
-        option1 = Button(main_window, text ="Wyświetl obraz", command = self.option1_callback)
-        option2 = Button(main_window, text ="Wypisz informacje o obrazie", command = self.option2_callback)
-        option3 = Button(main_window, text ="Wyświetl transformatę obrazu", command = self.option3_callback)
-        option4 = Button(main_window, text ="Zapisz obraz usuwając zbędne metadane", command = self.option4_callback)
-        option5 = Button(main_window, text ="Wyjdź", command = self.option5_callback)
+        b_width = 32
+        option1 = Button(main_window, width = b_width, text ="Wyświetl obraz",                          command = self.option1_callback)
+        option2 = Button(main_window, width = b_width, text ="Wypisz informacje o obrazie",             command = self.option2_callback)
+        option3 = Button(main_window, width = b_width, text ="Wyświetl transformatę obrazu",            command = self.option3_callback)
+        option4 = Button(main_window, width = b_width, text ="Zapisz obraz usuwając zbędne metadane",   command = self.option4_callback)
+        option5 = Button(main_window, width = b_width, text ="Wyjdź",                                   command = self.option5_callback)
         
         option1.pack()
         option2.pack()
@@ -31,6 +32,8 @@ class GuiMenu:
         option5.pack()
 
         main_window.mainloop()
+
+    ############################################################################################
 
     def option1_callback(self):
         show.show_image(self.pic_inf.file_name)
