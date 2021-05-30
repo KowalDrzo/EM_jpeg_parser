5837100686e814ccd18b05bf6d8275f2e251a513
KowalDrzo <248959@student.pwr.edu.pl>
2021-05-30 14:05:35 +0200

    Revert "Próby szyfrowania bloków (nie działa jeszcze))"
    This reverts commit 9d3c74212f1173f0d73d6aed4513ed30fd48518d.

--- szyfrowanie/szyfrowanie_RSA.py
+++ szyfrowanie/szyfrowanie_RSA.py
@@ -1,5 +1,5 @@
 import random
-from typing import List, Type
+from typing import Type
 import sympy
 from sympy.printing.codeprinter import print_fcode
 
@@ -129,20 +129,17 @@ def modular_inverse(a, b): #generuje
 
     return x
 
-def encrypt(public_key, N, bin_file) -> List[bytes]:
+def encrypt(public_key, N, bin_file):
     
     new_file = []
-    new_parts = []
     new_val = 0
-    parts_8_bytes = divide_chunks(bin_file, 8)
 
-    for part in parts_8_bytes:
+    for byte in bin_file:
 
-        c = int.from_bytes(part, byteorder="big")
-        new_val = pow(c, public_key, N)
-        new_parts += new_val.to_bytes(8, "big")
+        new_val = pow(byte, public_key, N)
+        new_file.append(new_val.to_bytes(8, "big"))
 
-    return new_parts
+    return new_file
 
 def divide_chunks(l, n):
       
@@ -150,16 +147,15 @@ def divide_chunks(l, n):
     for i in range(0, len(l), n): 
         yield l[i:i + n]
 
-def decrypt(private_key, N, bin_file) -> List[bytes]:
+def decrypt(private_key, N, bin_file):
     
     original_file = []
     parts_8_bytes = divide_chunks(bin_file, 8)
-    new_val = 0
 
+    #parts = cipher.split()
     for part in parts_8_bytes:
         c = int.from_bytes(part, byteorder="big")
-        new_val = pow(c, private_key, N)
-        original_file += new_val.to_bytes(8, "big")
+        original_file.append(pow(c, private_key, N))
 
     return original_file
 
@@ -181,7 +177,9 @@ enc = encrypt(public_key, N, bin_file)
 print("Zapisuję")
 
 file = open("SFR.jpg", "wb")
-file.write(bytes(enc))
+
+for byte in enc:
+    file.write(byte)
 file.close()
 
 print("Odszyfrowywuję")
